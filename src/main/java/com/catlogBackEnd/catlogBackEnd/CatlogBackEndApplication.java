47ba42656f3f0ce99517921a196ac84604b4f05a
Sohaib Badaha <sohaibbadaha@192.168.1.8>
2025-09-07 14:08:36 +0300

    ready to work

---
+++ src/main/java/com/catlogBackEnd/catlogBackEnd/CatlogBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.catlogBackEnd.catlogBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatlogBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatlogBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catlogBackEnd/catlogBackEnd/CatlogBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catlogBackEnd.catlogBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatlogBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
